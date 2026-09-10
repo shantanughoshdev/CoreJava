4afe27a8b84381ca989b12244eeb5b4a02243969
shantanughoshdev <shantanughoshdev@gmail.com>
2026-09-10 20:16:51 +0530

    Learn condition with loop

---
+++ src/com/shan/ConditionLoop/Loop.java
@@ -0,0 +1,36 @@
+package com.shan.ConditionLoop;
+
+public class Loop {
+    public static void main(String[] args) {
+
+        // While Loop
+//        int count = 1;
+//        while(count != 5){
+//            System.out.println(count);
+//            count++;
+//        }
+
+        //For Loop
+        for (int count = 1; count != 5; count++) {
+            System.out.println(count);
+            System.out.println("Shan");
+        }
+
+        //While
+        int i = 0;
+        while (i != 5) {
+            System.out.println("Shan");
+            i++;
+        }
+
+        //do while
+        System.out.println("----------------------");
+        int x = 0;
+        do {
+            System.out.println("Shan");
+            x++;
+        }while (x < 5);
+
+
+    }
+}
