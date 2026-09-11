d039e7a35dd99242b643f1c474e4df22eb017fbd
shantanughoshdev <shantanughoshdev@gmail.com>
2026-09-11 23:47:45 +0530

    Learned method with return and with argument

--- src/com/shan/Method/StringMethod.java
+++ src/com/shan/Method/StringMethod.java
@@ -1,14 +1,18 @@
 package com.shan.Method;
 
+import java.util.Scanner;
+
 public class StringMethod  {
     public static void main(String[] args) {
-        String result = greet();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = sc.next();
+        String result = greet(name);
         System.out.println(result);
     }
 
-    static String greet(){
-        String greeting = "Hello to my world";
-
-        return greeting;
+    static String greet(String name) {
+        return "Hello " + name;
     }
 }
