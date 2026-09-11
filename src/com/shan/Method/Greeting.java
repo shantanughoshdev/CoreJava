60ed788ef4828e45097676af80d0941eb814e773
shantanughoshdev <shantanughoshdev@gmail.com>
2026-09-11 16:47:12 +0530

    Learned program without return and without arguments

---
+++ src/com/shan/Method/Greeting.java
@@ -0,0 +1,11 @@
+package com.shan.Method;
+
+public class Greeting {
+    static void main() {
+        greeting();
+    }
+
+    static void greeting(){
+        System.out.println("Hello guys");
+    }
+}
