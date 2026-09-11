ebe5180e88a95348919e6b03c0da064c156976e6
shantanughoshdev <shantanughoshdev@gmail.com>
2026-09-11 16:33:08 +0530

    Learned program with return and without arguments

--- src/com/shan/Method/Sum.java
+++ src/com/shan/Method/Sum.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        sum();
+        int result = sum();
+        System.out.println("Sum of the two number : "+result);
     }
 
+    // with return type
 
-    static void sum() {
+    static int sum() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number 1 :");
         int num1 = sc.nextInt();
         System.out.println("Enter number 2 :");
         int num2 = sc.nextInt();
         int sum = num1 + num2;
-        System.out.println("The sum is " + sum);
+        return sum;
     }
 }
