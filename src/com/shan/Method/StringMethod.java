5bbcd7664360d4d59012de664d7934ff3e37e071
shantanughoshdev <shantanughoshdev@gmail.com>
2026-09-11 17:09:04 +0530

    Example of string method with retun and without argument

---
+++ src/com/shan/Method/StringMethod.java
@@ -0,0 +1,14 @@
+package com.shan.Method;
+
+public class StringMethod  {
+    public static void main(String[] args) {
+        String result = greet();
+        System.out.println(result);
+    }
+
+    static String greet(){
+        String greeting = "Hello to my world";
+
+        return greeting;
+    }
+}
